ca4169bc8087cb103312ac00d0151e2c8fa6b386
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-04-22 17:58:32 +0200

    Real Estate routing done

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OptionsController;
+use App\Http\Controllers\RealEstateController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,20 +22,65 @@ Route::get('/', [HomeController::class, 'index']);
 Route::prefix('/admin')
      ->name('admin.')
      ->group(function () {
-         Route::get('/', [AdminController::class, 'index'])->name('index');
+         Route::get('/', [AdminController::class, 'index'])
+              ->name('index')
+         ;
 
          Route::prefix('/options')
               ->name('options.')
               ->controller(OptionsController::class)
               ->group(function () {
                   // CRUD
-                  Route::get('/', 'index')->name('index');
-                  Route::get('/create', 'create')->name('create');
-                  Route::post('/create', 'store')->name('store');
-                  Route::get('/show/{option}', 'show')->name('show');
-                  Route::get('/edit/{option}', 'edit')->name('edit');
-                  Route::patch('/edit/{option}', 'update')->name('update');
-                  Route::delete('/delete/{option}', 'destroy')->name('destroy');
+                  Route::get('/', 'index')
+                       ->name('index')
+                  ;
+                  Route::get('/create', 'create')
+                       ->name('create')
+                  ;
+                  Route::post('/create', 'store')
+                       ->name('store')
+                  ;
+                  Route::get('/show/{option}', 'show')
+                       ->name('show')
+                  ;
+                  Route::get('/edit/{option}', 'edit')
+                       ->name('edit')
+                  ;
+                  Route::patch('/edit/{option}', 'update')
+                       ->name('update')
+                  ;
+                  Route::delete('/delete/{option}', 'destroy')
+                       ->name('destroy')
+                  ;
+              })
+         ;
+
+         Route::prefix('/realEstates')
+              ->name('realEstates.')
+              ->controller(RealEstateController::class)
+              ->group(function () {
+                  // CRUD
+                  Route::get('/', 'index')
+                       ->name('index')
+                  ;
+                  Route::get('/create', 'create')
+                       ->name('create')
+                  ;
+                  Route::post('/create', 'store')
+                       ->name('store')
+                  ;
+                  Route::get('/show/{realEstate}', 'show')
+                       ->name('show')
+                  ;
+                  Route::get('/edit/{realEstate}', 'edit')
+                       ->name('edit')
+                  ;
+                  Route::patch('/edit/{realEstate}', 'update')
+                       ->name('update')
+                  ;
+                  Route::delete('/delete/{realEstate}', 'destroy')
+                       ->name('destroy')
+                  ;
               })
          ;
      })
