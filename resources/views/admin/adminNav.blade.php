1c380944ea89c3041d9020e2d62f378fb0306e64
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-05-01 15:17:11 +0200

    upgraded adminNav.blade.php

--- resources/views/admin/adminNav.blade.php
+++ resources/views/admin/adminNav.blade.php
@@ -1,47 +1,54 @@
-@php
-	//	dump(
-	//        request()->routeIs('admin.options.create'),
-	//        request()->route()->getName(),
-	//        route('admin.options.create'),
-	//        route(request()->route()->getName()),
-	//	)
-@endphp
-
 <nav class="navbar navbar-expand-lg bg-dark">
 	<div class="container-fluid">
-		<a href="/" class="navbar-brand">Agence</a>
+		<a href="/" class="navbar-brand">Agency</a>
 		<button
-				class="navbar-toggler"
-				type="button"
-				data-bs-toggle="collapse"
-				data-bs-target="#navbarSupportedContent"
-				aria-controls="navbarSupportedContent"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarSupportedContent"
+			aria-controls="navbarSupportedContent"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
 		>
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-				<li class="nav-item"><a
-							href="{{ route('admin.index') }}"
-							@class([
-									'nav-link' => true,
-									'active' => request()->routeIs('admin.*') && !request()->routeIs('admin.options.*'),
-							])
+				<li class="nav-item dropdown">
+					<a
+						@class([
+							'nav-link',
+							'dropdown-toggle',
+							'active' => request()->routeIs('admin.realEstates.*'),
+						])
+						role="button"
+						data-bs-toggle="dropdown"
+						aria-expanded="false"
 					>
-						Gérer les biens
+						Real estate
 					</a>
+					<ul class="dropdown-menu">
+						<li><a class="dropdown-item" href="{{ route('admin.realEstates.index') }}">List</a></li>
+						<li><a class="dropdown-item" href="{{ route('admin.realEstates.create') }}">Create</a></li>
+					</ul>
 				</li>
-				<li class="nav-item"><a
-							href="{{ route('admin.options.index') }}"
-							@class([
-									'nav-link' => true,
-									'active' => request()->routeIs('admin.options.*'),
-							])
+				<li class="nav-item dropdown">
+					<a
+						@class([
+							'nav-link',
+							'dropdown-toggle',
+							'active' => request()->routeIs('admin.options.*'),
+						])
+						role="button"
+						data-bs-toggle="dropdown"
+						aria-expanded="false"
 					>
-						Gérer les options
+						Options
 					</a>
+					<ul class="dropdown-menu">
+						<li><a class="dropdown-item" href="{{ route('admin.options.index') }}">List</a></li>
+						<li><a class="dropdown-item" href="{{ route('admin.options.create') }}">Create</a></li>
+					</ul>
 				</li>
 			</ul>
 		</div>
